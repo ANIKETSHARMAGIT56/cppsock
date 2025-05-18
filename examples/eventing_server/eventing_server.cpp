240c874fb4e5f6707e67558c8f0187291d872ace
aniket <aniket@git.com>
2025-05-18 11:27:33 +0530

    added, eventing_server example

--- examples/eventing_server/eventing_server.cpp
+++ examples/eventing_server/eventing_server.cpp
@@ -9,12 +9,10 @@
 
 int main() {
     auto ws_Server =std::make_shared<cppsock::ws::ws_server_boost_impl>();
-    auto event_server= std::make_shared<cppsock::eventing::event_manager>(std::move(ws_Server),cppsock::eventing::eventing_peer{1,"hi","dfe"});
-
-
-    event_server->on_event("testing event", [](cppsock::eventing::event ev) {
+    cppsock::eventing::event_manager event_server(std::move(ws_Server),{1,"server","okay"});
+    event_server.on_event("testing event", [](cppsock::eventing::event ev) {
         std::cout << ev << std::endl;
     });
 
-    event_server->run(1831, 1);
+    event_server.run(1831, 1);
 }
